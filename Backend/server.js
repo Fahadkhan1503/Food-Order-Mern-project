612190aca88b8d1f291788bc8bea50800f005d2f
hamza0426 <muhammadhamzaowais5@gmail.com>
2025-02-05 15:30:25 +0500

    Database Connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 import app from "./app.js";
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log(`Server Running on PORT ${process.env.PORT}`);
-<<<<<<< HEAD
+// <<<<<<< HEAD
 });
-=======
-});
->>>>>>> 6148247fd88d58699ee2d16217c06490f2d2ca94
+// =======
+// });
+// >>>>>>> 6148247fd88d58699ee2d16217c06490f2d2ca94
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 All Project related Details
+
+Project has just been started
+
+we have completed the database related working
+now working on error handling...
