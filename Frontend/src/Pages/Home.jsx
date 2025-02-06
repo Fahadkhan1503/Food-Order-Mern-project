1a4f119d2a61ff576e1a48c119d32fe5b2c3244b
Fahad Khan <fahad.g23402@iqra.edu.pk>
2025-02-06 17:52:07 +0500

    home and about section completed

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+import HeroSection from '../components/HeroSection'
+import About from '../components/About'
+
+
+const Home = () => {
+  return (
+    <>
+        <h1>
+            <HeroSection/>
+            <About/>
+        </h1>
+    </>
+  )
+}
+
+export default Home
---
+++ Frontend/src/Pages/Success.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <>
+        <h1>
+            SUCCESS PAGE
+        </h1>
+    </>
+  )
+}
+
+export default Success
