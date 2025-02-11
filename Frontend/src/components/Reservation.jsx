a98c5d2df565678848a9507114e8d744375c667e
Fahadkhan1503 <fahadkhan1503@gmail.com>
2025-02-11 18:25:46 +0500

    Updated Reservation.jsx

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -16,7 +16,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://food-order-mern-project-backend.vercel.app/reservation/send",
+        "https://food-order-mern-project-backend.vercel.app/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
